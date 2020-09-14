08ec042fc5a553b2af1bc3b37d8489c4e715cb54
84h3p <vano0448@gmail.com>
2020-09-14 23:38:33 +0600

    new file:   fonts/main.otf 	new file:   img/logo.jpg 	new file:   img/logo2.jpeg 	new file:   index.html 	new file:   index2.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,17 @@
+function printForm(e){
+    
+    // получаем значение поля key
+    var keyBox = document.search.key;
+    var val = keyBox.value;
+    // получаем элемент printBlock
+    var printBlock = document.getElementById("printBlock");
+    // создаем новый параграф
+    var pElement = document.createElement("p");
+    // устанавливаем у него текст
+    pElement.textContent = val + 'цетопс' ;
+    // добавляем параграф в printBlock
+    printBlock.appendChild(pElement);
+}
+
+var printButton = document.search.print;
+printButton.addEventListener("click", printForm);

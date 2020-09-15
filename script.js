c1de232e4e9d97349120a57589778f9d4a6fc111
84h3p <vano0448@gmail.com>
2020-09-15 16:55:02 +0600

    [Hit return to continue] 	modified:   index.html 	deleted:    index2.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function printForm(e){
-    
+
     // получаем значение поля key
     var keyBox = document.search.key;
     var val = keyBox.value;
     // получаем элемент printBlock
     var printBlock = document.getElementById("printBlock");
+
+    // стираем уже существующий текст
+    printBlock.innerHTML = '';
+
     // создаем новый параграф
     var pElement = document.createElement("p");
     // устанавливаем у него текст
-    pElement.textContent = val + 'цетопс' ;
+    pElement.textContent = val + 'цетопс';
     // добавляем параграф в printBlock
     printBlock.appendChild(pElement);
 }
 
-var printButton = document.search.print;
-printButton.addEventListener("click", printForm);
+
+    var printButton = document.search.print;
+    printButton.addEventListener("click", printForm);

de5805da0242804ae0617608255f85a439458faa
84h3p <vano0448@gmail.com>
2020-10-05 11:24:25 +0600

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-function printForm(e){
-  let fruits = ["б", "в", "г", "д", "ж", "з",
-  "й", "л", "м", "н", "р", "п", "ф", "к", "т",
-   "ш", "с", "х", "ц", "ч", "щ"]
+const dictionary = ['цетопс', 'дактиль', 'завр']
+let dinos = dictionary.slice(0)
 
-   let dinos = ['цетопс', 'дактиль', 'завр']
-   var dino = dinos[Math.floor(Math.random()*dinos.length)];
+const consonant = ["б", "в", "г", "д", "ж", "з",
+     "й", "л", "м", "н", "р", "п", "ф", "к", "т",
+     "ш", "с", "х", "ц", "ч", "щ"]
+
+// получаем элемент printBlock
+const printBlock = document.getElementById("printBlock");
+
+// получаем значение поля key
+const keyBox = document.search.key;
+
+function printForm(event) {
+     const index = Math.floor(Math.random()*dinos.length)
+     const dino = dinos[index];
+
+     // убираем выбранный элемент
+     dinos.splice(index, 1)
+
+     if (dinos.length === 0) {
+          dinos = dictionary.slice(0)
+          dinos.splice(dinos.indexOf(dino), 1)
+     }
 
 
-    // получаем значение поля key
-    var keyBox = document.search.key;
-    var val = keyBox.value;
-    // получаем элемент printBlock
-    var printBlock = document.getElementById("printBlock");
+     const val = keyBox.value;
 
-    // стираем уже существующий текст
-    printBlock.innerHTML = '';
+     // стираем уже существующий текст
+     printBlock.innerHTML = '';
 
-    // создаем новый параграф
-    var pElement = document.createElement("p");
+     // создаем новый параграф
+     const pElement = document.createElement("p");
 
-    var endLet = (val.charAt(val.length -1))
-    // устанавливаем у него текст
-    if (fruits.includes(endLet)) {
-      pElement.textContent = val + 'о' + dino;
-    }
-    else if (val.length = 0) {
-      pElement.textContent = 'Нету имени';
-    }
-    else {
-      pElement.textContent = val + dino;
-    }
+     const endLet = (val.charAt(val.length - 1))
+     // устанавливаем у него текст
+     if (consonant.includes(endLet)) {
+          pElement.textContent = val + 'о' + dino;
+     }
+     else if (val.length === 0) {
+          pElement.textContent = 'Нету имени';
+     }
+     else {
+          pElement.textContent = val + dino;
+     }
 
-    // добавляем параграф в printBlock
-    printBlock.appendChild(pElement);
+     // добавляем параграф в printBlock
+     printBlock.appendChild(pElement);
 }
 
 
-    var printButton = document.search.print;
-  //  printButton.addEventListener("click", printForm);
-    printButton.addEventListener("click", printForm)
+let printButton = document.search.print;
+//  printButton.addEventListener("click", printForm);
+printButton.addEventListener("click", printForm)
 
-    search.addEventListener('submit', (event) => event.preventDefault())
+search.addEventListener('submit', (event) => event.preventDefault())
